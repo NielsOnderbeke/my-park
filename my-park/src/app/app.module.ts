4a425f14d1d2011a018d5f489cc12a06c7e4fa80
NielsOnderbeke <niels.onderbeke.no@gmail.com>
2023-12-24 10:37:34 +0100

    feat: init + basic data fetching

--- my-park/src/app/app.module.ts
+++ my-park/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapViewComponent } from './map-view/map-view.component';
+import { LocationListComponent } from './location-list/location-list.component';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapViewComponent,
+    LocationListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
